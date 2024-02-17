b0aaab9ed465e4e72052bfa4c5f09801b2c9e8c7
ビビ <38562978+0xOptimizer@users.noreply.github.com>
2024-02-17 12:28:13 +0800

    update: small changes to post design

--- public/js/post-feed.js
+++ public/js/post-feed.js
@@ -522,9 +522,9 @@ $(document).ready(function () {
                     <div class="post-content-trimmed-txt">${post_message}</div><div class="post-content-complete-text" style="display: none;">${fPost_message}</div>
                 </div>
                 <div class="user_post_attachements">${post_attachment}</div>
-                <div class="mt-2 justify-content-start d-flex flex-row ff-primary-light">
+                <div class="mt-2 d-flex flex-row justify-content-start ff-primary-light">
                     <div class="ff-primary-regular">
-                        <button type="button" class="btn btn-secondary post-react-btn" data-react="like">
+                        <button type="button" class="btn btn-secondary post-react-btn" data-react="like" style="width: 64px;">
                             <svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <g>
                                     <path fill="none" d="M0 0h24v24H0z"></path>
@@ -533,7 +533,7 @@ $(document).ready(function () {
                             </svg>
                             <span>${react_count[0]}</span>
                         </button>
-                        <button type="button" class="btn btn-secondary post-react-btn" data-react="like">
+                        <button type="button" class="btn btn-secondary post-react-btn" data-react="haha" style="width: 64px;">
                             <svg width="20px" height="20px" viewBox="0 0 1500 1500" xmlns="http://www.w3.org/2000/svg">
                                 <path class="st0" d="M542.7 1092.6H377.6c-13 0-23.6-10.6-23.6-23.6V689.9c0-13 10.6-23.6 23.6-23.6h165.1c13 0 23.6 10.6 23.6 23.6V1069c0 13-10.6 23.6-23.6 23.6zM624 1003.5V731.9c0-66.3 18.9-132.9 54.1-189.2 21.5-34.4 69.7-89.5 96.7-118 6-6.4 27.8-25.2 27.8-35.5 0-13.2 1.5-34.5 2-74.2.3-25.2 20.8-45.9 46-45.7h1.1c44.1 1 58.3 41.7 58.3 41.7s37.7 74.4 2.5 165.4c-29.7 76.9-35.7 83.1-35.7 83.1s-9.6 13.9 20.8 13.3c0 0 185.6-.8 192-.8 13.7 0 57.4 12.5 54.9 68.2-1.8 41.2-27.4 55.6-40.5 60.3-2.6.9-2.9 4.5-.5 5.9 13.4 7.8 40.8 27.5 40.2 57.7-.8 36.6-15.5 50.1-46.1 58.5-2.8.8-3.3 4.5-.8 5.9 11.6 6.6 31.5 22.7 30.3 55.3-1.2 33.2-25.2 44.9-38.3 48.9-2.6.8-3.1 4.2-.8 5.8 8.3 5.7 20.6 18.6 20 45.1-.3 14-5 24.2-10.9 31.5-9.3 11.5-23.9 17.5-38.7 17.6l-411.8.8c-.2 0-22.6 0-22.6-30z"></path>
                                 <path class="st0" d="M750 541.9C716.5 338.7 319.5 323.2 319.5 628c0 270.1 430.5 519.1 430.5 519.1s430.5-252.3 430.5-519.1c0-304.8-397-289.3-430.5-86.1z"></path>
@@ -548,7 +548,7 @@ $(document).ready(function () {
                             </svg>
                             <span>${react_count[1]}</span>
                         </button>
-                        <button type="button" class="btn btn-secondary post-react-btn" data-react="like">
+                        <button type="button" class="btn btn-secondary post-react-btn" data-react="heart" style="width: 64px;">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 544.582 544.582" style="enable-background:new 0 0 544.582 544.582;" xml:space="preserve">
                                 <g>
                                     <path fill="#ff0025" color="#ff0025" d="M448.069,57.839c-72.675-23.562-150.781,15.759-175.721,87.898C247.41,73.522,169.303,34.277,96.628,57.839C23.111,81.784-16.975,160.885,6.894,234.708c22.95,70.38,235.773,258.876,263.006,258.876c27.234,0,244.801-188.267,267.751-258.876C561.595,160.732,521.509,81.631,448.069,57.839z"></path>
@@ -556,9 +556,11 @@ $(document).ready(function () {
                             </svg>
                             <span>${react_count[2]}</span>
                         </button>
-                        <button type="button" class="btn btn-secondary show_comment_section" data-post_id="${posts.post_id}" data-comments_count="${comment_count}"><i class="bi bi-chat-quote-fill"></i> ${comment_count}</button>
-                        <button type="button" class="btn btn-secondary share_this_post" data-post_id="${posts.post_id}"><i class="bi bi-share-fill"></i></button>
-                        <button type="button" class="btn btn-secondary show_comment_section" data-post_id="${posts.post_id}" data-comments_count="${comment_count}" style="float: right;"><i class="bi bi-reply-fill"></i> Reply</button>
+                        <button type="button" class="btn btn-secondary show_comment_section" data-post_id="${posts.post_id}" data-comments_count="${comment_count}" style="width: 64px;"><i class="bi bi-chat-quote-fill"></i> ${comment_count}</button>
+                        <div style="display: inline-block; float: right;">
+                            <button type="button" class="btn btn-secondary share_this_post" data-post_id="${posts.post_id}"><i class="bi bi-share-fill" style="width: 64px;"></i></button>
+                            <button type="button" class="btn btn-secondary show_reply_section" data-post_id="${posts.post_id}" data-comments_count="${comment_count}" style="float: right;"><i class="bi bi-reply-fill"></i> Reply</button>
+                        </div>
                     </div>
                 </div>
             </div>
